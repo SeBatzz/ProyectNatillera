4c9c2629a05257557fe5ebad846e6548db215a26
BATCOMPUTER\Sebastian <Sebastianrr1390@gmail.com>
2025-05-28 01:03:14 -0500

    Add project files.

---
+++ Clases/ClsCliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProyectNatillera.Models;
+
+namespace ProyectNatillera.Clases
+{
+    public class ClsCliente
+    {
+        public NatilleraDBEntities NatilleraDB = new NatilleraDBEntities();
+        public Cliente Cliente { get; set; }
+
+        public Cliente ConsultarXDocumento(string Documento)
+        {
+
+            Cliente Cl = NatilleraDB.Clientes.FirstOrDefault(e => e.DocumentoCliente == Documento);
+            return Cl;
+        }
+    }
+}
---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProyectNatillera.Models;
+using ProyectNatillera.Clases;
+using System.Web.Http;
+
+namespace ProyectNatillera.Controllers
+{
+    [RoutePrefix("api/Clientes")]
+    
+    public class ClientesController : ApiController
+    {
+
+        [HttpGet]
+        [Route("ConsultarXDocumento")]
+        public Cliente ConsultarXDocumento(string Documento)
+        {
+            ClsCliente cliente = new ClsCliente();
+            return cliente.ConsultarXDocumento(Documento);
+        }
+    }
+}
