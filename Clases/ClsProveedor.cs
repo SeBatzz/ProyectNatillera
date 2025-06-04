141a45b06e9436e3e2cc3779a7dfaa8d0624d3e5
IsabelaCuartas <mariacuartas330373@correo.itm.edu.co>
2025-06-03 22:07:16 -0500

    Añadir clase proveedor

---
+++ Clases/ClsProveedor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using ProyectNatillera.Models;
+
+namespace ProyectNatillera.Clases
+{
+    public class ClsProveedor
+    {
+        public NatilleraDBEntities NatilleraDB = new NatilleraDBEntities();
+        public Proveedor proveedor { get; set; }
+
+        public Proveedor ConsultarXId(int id)
+        {
+            return NatilleraDB.Proveedores.FirstOrDefault(x => x.idProveedor == id);
+        }
+
+        public List<Proveedor> ConsultarTodos()
+        {
+            return NatilleraDB.Proveedores
+                .OrderBy(x => x.NombreProveedor)
+                .ToList();
+        }
+
+        public string Insertar()
+        {
+            try
+            {
+                if (proveedor == null)
+                {
+                    return "Error: No se proporcionó un objeto Proveedor para insertar.";
+                }
+
+                NatilleraDB.Proveedores.Add(proveedor);
+                NatilleraDB.SaveChanges();
+
+                return "Proveedor insertado correctamente.";
+            }
+            catch (Exception ex)
+            {
+                return "Error al insertar el proveedor: " + ex.Message;
+            }
+        }
+
+        public string Actualizar()
+        {
+            if (proveedor == null)
+            {
+                return "Error: No se proporcionó un objeto Proveedor para actualizar.";
+            }
+
+            Proveedor pr = ConsultarXId(proveedor.idProveedor);
+
+            if (pr == null)
+            {
+                return "Error: El ID del proveedor no es válido o no existe.";
+            }
+
+            try
+            {
+                NatilleraDB.Proveedores.AddOrUpdate(proveedor);
+                NatilleraDB.SaveChanges();
+
+                return "Proveedor actualizado correctamente.";
+            }
+            catch (Exception ex)
+            {
+                return "Error al actualizar el proveedor: " + ex.Message;
+            }
+        }
+
+        public string EliminarXId(int id)
+        {
+            try
+            {
+                Proveedor pr = ConsultarXId(id);
+
+                if (pr == null)
+                {
+                    return "Error: Proveedor no encontrado con el ID proporcionado.";
+                }
+
+                NatilleraDB.Proveedores.Remove(pr);
+                NatilleraDB.SaveChanges();
+
+                return "Proveedor eliminado correctamente.";
+            }
+            catch (Exception ex)
+            {
+                return "Error al eliminar el proveedor: " + ex.Message;
+            }
+        }
+
+
+    }
+}
