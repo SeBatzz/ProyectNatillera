712ce350494ef05ff2f6474e491d5b390296a4e5
BATCOMPUTER\Sebastian <Sebastianrr1390@gmail.com>
2025-06-04 00:39:59 -0500

    Controladres varios

---
+++ Controllers/DepartamentosController.cs
@@ -0,0 +1,67 @@
+﻿using ProyectNatillera.Clases;
+using ProyectNatillera.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+
+namespace ProyectNatillera.Controllers
+{
+    [RoutePrefix("api/Departamentos")]
+    public class DepartamentosController : ApiController
+    {
+
+        [HttpGet]
+        [Route("ConsultarXId")]
+        public Departamento ConsultarXId(int Id)
+        {
+            ClsDepartamento departamento = new ClsDepartamento();
+            return departamento.ConsultarXId(Id);
+        }
+
+        [HttpGet]
+        [Route("ConsultarTodos")]
+        public List<Departamento> ConsultarTodos()
+        {
+            ClsDepartamento departamento = new ClsDepartamento();
+            return departamento.ConsultarTodos();
+        }
+
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar([FromBody] Departamento D)
+        {
+            if (D == null)
+            {
+                return "Error: Los datos del Prestamo son nulos.";
+            }
+            ClsDepartamento departamento = new ClsDepartamento();
+            departamento.departamento = D;
+            return departamento.Insertar();
+        }
+
+        [HttpPut]
+        [Route("Actualizar")]
+        public string Actualizar([FromBody] Departamento D)
+        {
+            if (D == null)
+            {
+                return "Error: Los datos del Prestamo son nulos.";
+            }
+            ClsDepartamento departamento = new ClsDepartamento();
+            departamento.departamento = D;
+            return departamento.Actualizar();
+        }
+
+        [HttpDelete]
+        [Route("EliminarXId")]
+        public string EliminarXId(int Id)
+        {
+            ClsDepartamento departamento = new ClsDepartamento();
+            return departamento.EliminarXId(Id);
+        }
+
+    }
+}
--- Controllers/EventosController.cs
+++ Controllers/EventosController.cs
@@ -2,7 +2,7 @@
 using ProyectNatillera.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq;  
 using System.Web;
 using System.Web.Http;
 
